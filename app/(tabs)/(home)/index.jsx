300f842fbb11581e3f384160e08f2e4c78ef85b9
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-10-02 21:53:48 +0200

    index

--- app/(tabs)/(home)/index.jsx
+++ app/(tabs)/(home)/index.jsx
@@ -21,14 +21,14 @@ export default function Hero() {
   return (
     <ScrollView style={styles.container}>
       {/* Navigation Links */}
-      <View style={styles.containerA}>
-        <Link href="/" style={styles.paragraphA}>
+      <View style={styles.navigationContainer}>
+        <Link href="/" style={styles.navLink}>
           Home
         </Link>
-        <Link href="/contact" style={styles.paragraphA}>
+        <Link href="/contact" style={styles.navLink}>
           Contact
         </Link>
-        <Link href="/about" style={styles.paragraphA}>
+        <Link href="/about" style={styles.navLink}>
           About
         </Link>
       </View>
@@ -46,34 +46,11 @@ export default function Hero() {
           isMuted
         />
 
-        {/* Gradient Overlay with Title and Subtitle */}
+        {/* Gradient Overlay with Buttons */}
         <LinearGradient
           colors={["rgba(0,0,0,0.8)", "transparent"]}
           style={[styles.overlay, { width: screenWidth }]}
         >
-          <View style={styles.overlayContent}>
-            <Text style={styles.title}>Advanced Beauty Solutions</Text>
-            <Text style={styles.subtitle}>
-              Visit our luxurious salon in the heart of the UAE to discover a
-              world where brilliance and beauty meet.
-            </Text>
-          </View>
-        </LinearGradient>
-      </View>
-
-      {/* Action Buttons */}
-      <ImageBackground
-        source={{
-          uri: "https://res.cloudinary.com/dzzm7ye56/image/upload/v1727125377/insta01_uyu4hh.jpg",
-        }}
-        style={styles.imageBackground}
-        resizeMode="cover"
-      >
-        <LinearGradient
-          colors={["rgba(0,0,0,0.6)", "rgba(0,0,0,0.6)"]}
-          style={styles.overlayContent}
-        >
-          {/* Action Buttons */}
           <View style={styles.buttonContainer}>
             <TouchableOpacity
               style={styles.button}
@@ -97,9 +74,31 @@ export default function Hero() {
             </TouchableOpacity>
           </View>
         </LinearGradient>
+      </View>
 
-        {/* Content Section */}
+      {/* Image Background Section with Gradient and Title */}
+      <ImageBackground
+        source={{
+          uri: "https://res.cloudinary.com/dzzm7ye56/image/upload/v1727125377/insta01_uyu4hh.jpg",
+        }}
+        style={styles.imageBackground}
+        resizeMode="cover"
+      >
+        <LinearGradient
+          colors={["rgba(0,0,0,0.6)", "rgba(0,0,0,0.6)"]}
+          style={styles.overlayContent}
+        >
+          <View style={styles.overlayContent}>
+            <Text style={styles.title}>Advanced Beauty Solutions</Text>
+            <Text style={styles.subtitle}>
+              Visit our luxurious salon in the heart of the UAE to discover a
+              world where brilliance and beauty meet.
+            </Text>
+          </View>
+        </LinearGradient>
       </ImageBackground>
+
+      {/* Content Section */}
       <ImageBackground
         source={{
           uri: "https://res.cloudinary.com/dzzm7ye56/image/upload/v1727125329/insta02_ket2ej.jpg",
@@ -113,7 +112,7 @@ export default function Hero() {
         >
           <View style={styles.content}>
             <Text style={styles.mainTitle}>
-              Your Beauty <Text style={styles.endTitle}>Haven</Text>
+              Your Beauty <Text style={styles.highlightText}>Haven</Text>
             </Text>
             <Text style={styles.paragraph}>
               Visit our luxurious salon in the heart of the UAE to discover a
@@ -157,35 +156,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   overlayContent: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 50,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: colors.textColor,
-    textAlign: "center",
-    marginBottom: 5,
-  },
-  subtitle: {
-    fontSize: 18,
-    color: colors.lightGray,
-    textAlign: "center",
-    marginBottom: 20,
+    flexDirection: "column", // Stack buttons or content vertically
+    justifyContent: "flex-end", // Align content at the bottom
+    alignItems: "center", // Center content horizontally
+    paddingBottom: 30, // Adjust padding to position elements
+    gap: 15, // Space between the buttons or content
+    height: 600,
   },
   buttonContainer: {
-    flexDirection: "col", // Keep buttons side by side
+    flexDirection: "row", // Keep buttons side by side
     justifyContent: "center", // Center the buttons
     alignItems: "center",
-    paddingVertical: 200, // Spacing around the buttons
+    paddingVertical: 20, // Spacing around the buttons
     gap: 15, // Space between the buttons
   },
   button: {
-    backgroundColor: colors.teal950, // Bright green for the Book Now button
-    paddingHorizontal: 35, // Better horizontal padding for larger touch area
-    paddingVertical: 14, // Increased vertical padding for comfort
+    backgroundColor: colors.teal950, // Dark green for the Book Now button
+    paddingHorizontal: 35, // Horizontal padding for larger touch area
+    paddingVertical: 14, // Vertical padding for comfort
     borderRadius: 30, // More rounded corners for a modern feel
     borderWidth: 2, // Add a border for emphasis
     borderColor: colors.customGreen, // Border color to make it pop
@@ -196,7 +184,7 @@ const styles = StyleSheet.create({
     shadowRadius: 6, // Larger shadow radius for smooth effect
     alignItems: "center",
     justifyContent: "center",
-    marginBottom: 10, // Space at the bottom
+    // marginBottom: 10, // Space at the bottom
   },
   buttonText: {
     color: colors.textHover, // White text for contrast
@@ -211,7 +199,7 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     borderRadius: 50, // Fully rounded corners for a modern, icon-like look
     borderWidth: 2, // Border around the WhatsApp button
-    borderColor: colors.customGreen, // Light green border for contrast
+    borderColor: colors.customGreen, // Green border for contrast
     elevation: 5,
     shadowColor: "#000", // Shadow for WhatsApp button as well
     shadowOffset: { width: 2, height: 2 },
@@ -223,14 +211,30 @@ const styles = StyleSheet.create({
   content: {
     padding: 20,
   },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: colors.textColor, // Use a contrasting text color
+    textAlign: "center", // Center the text
+    marginBottom: 10, // Space below the title
+    paddingHorizontal: 20, // Add some padding for better readability
+  },
+  subtitle: {
+    fontSize: 18,
+    color: colors.lightGray, // Slightly lighter color for subtitle
+    textAlign: "center", // Center the text
+    lineHeight: 24, // Adjust line height for readability
+    paddingHorizontal: 20, // Padding for better alignment
+    marginBottom: 20, // Space below the subtitle
+  },
   mainTitle: {
     fontSize: 24,
     fontWeight: "bold",
     color: colors.textColor,
     marginBottom: 10,
   },
-  endTitle: {
-    color: colors.header2,
+  highlightText: {
+    color: colors.header2, // Special color for highlighting part of the text
   },
   paragraph: {
     fontSize: 16,
@@ -238,7 +242,7 @@ const styles = StyleSheet.create({
     textAlign: "justify",
     lineHeight: 24,
   },
-  containerA: {
+  navigationContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
@@ -247,11 +251,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: colors.oliveSoftGrayGreen,
   },
-  paragraphA: {
+  navLink: {
     fontSize: 18,
     color: colors.textColor,
     textAlign: "center",
     marginHorizontal: 10,
     textTransform: "uppercase",
+  },
+  imageBackground: {
+    width: "100%",
+    height: "auto",
   },
 });
